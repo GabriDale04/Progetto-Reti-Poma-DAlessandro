a849f8702774e41571499ef62e675f2d1a55c7e8
Tommaso Poma <tommaso.poma@itsrizzoli.it>
2024-12-21 15:32:24 +0100

    Project setup

---
+++ client/client.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, I'm your client!\n");
+    return 0;
+}
---
+++ server/server.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, I'm your server!\n");
+    return 0;
+}
