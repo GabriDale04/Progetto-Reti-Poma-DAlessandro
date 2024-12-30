672136dfc3a39dbdefef346abdaccd3199ca138d
GabriDale04 <gabri.dalessandro04@gmail.com>
2024-12-30 17:54:52 +0100

    print caratteri mappa

--- server/server.c
+++ server/server.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
+void printItem(int item)
+{
+    if (item == 0)
+        printf(" ");
+    else if (item == 1)
+        printf("■");
+    else if (item == 2)
+        printf("\033[31m🍎\033[0m");
+    else if (item == 3)
+        printf("\033[33m🍌\033[0m");
+    else if (item == 4)
+        printf("\033[35m🍇\033[0m");
+}
+
 int main() {
     printf("Hello, I'm your server!\n");
     return 0;
