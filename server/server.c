fb6c8e7c4961ed6de7b7c8a1c9550cbc1482e556
Tommaso Poma <tommaso.poma@itsrizzoli.it>
2025-01-02 18:24:43 +0100

    [server] try using parser on received messages

--- server/server.c
+++ server/server.c
@@ -20,71 +20,6 @@ void error(const char* msg) {
     exit(1);
 }
 
-void* clientThreadHandler(void* socket) {
-    int* socketPtr = (unsigned int*)socket;
-    int clientSocket = *socketPtr;
-
-    printf("Accepted new client\n");
-
-    char buffer[256];
-    int result;
-
-    while (1) {
-        bzero(buffer, 256);
-
-        result = read(clientSocket, buffer, 255);
-        if (result < 0) {
-            error("ERROR reading from socket");
-        }
-        else if (result == 0) {
-            printf("Client disconnected\n");
-            break;
-        }
-
-        buffer[strcspn(buffer, "\n")] = 0; // Rimuovi il \n "newline" se presente
-        printf("Here is the message: %s\n", buffer);
-
-        char response[] = "Message recived!";
-        int responseLength = strlen(response);
-        result = write(clientSocket, response, responseLength);
-        if (result < 0) {
-            error("ERROR writing to socket");
-        }
-    }
-
-    close(clientSocket);
-
-    return NULL;
-}
-
-void acceptloop(int serverSocket) {
-    struct sockaddr_in clientSocket;
-    socklen_t clientSocketLength = sizeof(clientSocket);
-    pthread_t clientThread;
-
-    while (1) {
-        int clientSocket = accept(serverSocket, (struct sockaddr*)&clientSocket, &clientSocketLength);
-        if (clientSocket < 0) {
-            error("ERROR on accept");
-        }
-        else {
-            pthread_create(&clientThread, NULL, &clientThreadHandler, &clientSocket);
-        }
-    }
-}
-
-void createMap() {
-    map = (int**)calloc(MAP_HEIGHT, sizeof(int*));
-
-    for (int i = 0; i < MAP_HEIGHT; i++)
-        map[i] = (int*)calloc(MAP_WIDTH, sizeof(int));
-
-    for (int r = 0; r < MAP_HEIGHT; r++)
-        for (int c = 0; c < MAP_WIDTH; c++)
-            if (r == 0 || r == MAP_HEIGHT - 1 || c == 0 || c == MAP_WIDTH - 1)
-                map[r][c] = 1;
-}
-
 void moveLeft(int distance) {
     printf("Eseguo movimento a sinistra di: %d\n", distance);
 }
@@ -156,6 +91,73 @@ void parse_command(const char* input) {
     }
 }
 
+void* clientThreadHandler(void* socket) {
+    int* socketPtr = (unsigned int*)socket;
+    int clientSocket = *socketPtr;
+
+    printf("Accepted new client\n");
+
+    char buffer[256];
+    int result;
+
+    while (1) {
+        bzero(buffer, 256);
+
+        result = read(clientSocket, buffer, 255);
+        if (result < 0) {
+            error("ERROR reading from socket");
+        }
+        else if (result == 0) {
+            printf("Client disconnected\n");
+            break;
+        }
+
+        buffer[strcspn(buffer, "\n")] = 0; // Rimuovi il \n "newline" se presente
+        printf("Here is the message: %s\n", buffer);
+
+        parse_command(buffer);
+
+        char response[] = "Message recived!";
+        int responseLength = strlen(response);
+        result = write(clientSocket, response, responseLength);
+        if (result < 0) {
+            error("ERROR writing to socket");
+        }
+    }
+
+    close(clientSocket);
+
+    return NULL;
+}
+
+void acceptloop(int serverSocket) {
+    struct sockaddr_in clientSocket;
+    socklen_t clientSocketLength = sizeof(clientSocket);
+    pthread_t clientThread;
+
+    while (1) {
+        int clientSocket = accept(serverSocket, (struct sockaddr*)&clientSocket, &clientSocketLength);
+        if (clientSocket < 0) {
+            error("ERROR on accept");
+        }
+        else {
+            pthread_create(&clientThread, NULL, &clientThreadHandler, &clientSocket);
+        }
+    }
+}
+
+void createMap() {
+    map = (int**)calloc(MAP_HEIGHT, sizeof(int*));
+
+    for (int i = 0; i < MAP_HEIGHT; i++)
+        map[i] = (int*)calloc(MAP_WIDTH, sizeof(int));
+
+    for (int r = 0; r < MAP_HEIGHT; r++)
+        for (int c = 0; c < MAP_WIDTH; c++)
+            if (r == 0 || r == MAP_HEIGHT - 1 || c == 0 || c == MAP_WIDTH - 1)
+                map[r][c] = 1;
+}
+
 int main(int argc, char* argv[]) {
 
     if (argc < 2) {
@@ -188,9 +190,6 @@ int main(int argc, char* argv[]) {
     acceptloop(serverSocket);
 
     close(serverSocket);
-
-    //char* input = "moveLeft,10";
-    //parse_command(input);
 
     return 0;
 }
